569d7aa01a64daa9dffb2ae5d42b7daf3ea0b068
AbithanP <abithan.mayo@gmail.com>
2026-01-07 10:47:17 -0500

    Added cube

---
+++ Cube.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Cube here.
+ * 
+ * @author Abithan Paskaranathan
+ * @version (a version number or a date)
+ */
+public class Cube extends SuperSmoothMover
+{
+    public static final int jumpHeight = 0;
+    
+    public void act()
+    {
+        move(2);
+        
+        if (Greenfoot.isKeyDown("space"))
+        {
+            jump();
+        }
+    }
+    
+    private void jump()
+    {
+        
+    }
+}
