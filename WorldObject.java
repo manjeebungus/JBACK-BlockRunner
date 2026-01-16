1a741a00616369a5f4c82dbe57672b5027c42097
Kelton <keltonkuan@gmail.com>
2026-01-16 10:41:39 -0500

    more preloading

--- WorldObject.java
+++ WorldObject.java
@@ -11,34 +11,23 @@ public abstract class WorldObject extends SuperSmoothMover {
     // World coordinates (pixels)
     protected double worldX;
     protected double worldY;
+    protected int preload;
 
     public WorldObject(double x, double y) {
         this.worldX = x;
         this.worldY = y;
+        this.preload = 64;
+    }
+    
+    public WorldObject(double x, double y, int preload) {
+        this.worldX = x;
+        this.worldY = y;
+        this.preload = preload;
     }
 
     // Update screen position based on camera
     public void updateScreenPosition(double camX, double camY, int screenWidth, int screenHeight) {
-        int preload = 64; // load tiles earlier
-    
-        double screenX = worldX - camX;
-        double screenY = worldY - camY;
-    
-        // Expand visibility check by preload value
-        if (screenX + getImage().getWidth() / 2 < -preload ||
-            screenX - getImage().getWidth() / 2 > screenWidth + preload ||
-            screenY + getImage().getHeight() / 2 < -preload ||
-            screenY - getImage().getHeight() / 2 > screenHeight + preload) {
-    
-            setLocation(-100, -100); // hide off-screen
-        } else {
-            setLocation(screenX + getImage().getWidth() / 2,
-                        screenY + getImage().getHeight() / 2);
-        }
-    }
-    
-    // Update screen position based on camera
-    public void updateScreenPosition(double camX, double camY, int screenWidth, int screenHeight, int preload) {
+        
     
         double screenX = worldX - camX;
         double screenY = worldY - camY;
